14d142a818375e5cc11d40353c6d4238560a3536
root <root@vps-a35dc061.vps.ovh.net.novalocal>
2021-06-07 18:35:39 +0000

    main page

--- website/models.py
+++ website/models.py
@@ -1,5 +1,6 @@
+from django.core.validators import FileExtensionValidator
 from django.db import models
 
 
 class Document(models.Model):
-    docfile = models.FileField(upload_to='documents/%Y/%m/%d')
+    docfile = models.FileField(upload_to='documents/%Y/%m/%d'])
