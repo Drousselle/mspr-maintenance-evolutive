79ef5837e20e89ba93f050e3ed7e7eaee7805620
Ubuntu <ubuntu@vps-a35dc061.vps.ovh.net.novalocal>
2021-06-07 19:10:33 +0000

    fix flake8

--- website/models.py
+++ website/models.py
@@ -1,6 +1,5 @@
-from django.core.validators import FileExtensionValidator
 from django.db import models
 
 
 class Document(models.Model):
-    docfile = models.FileField(upload_to='documents/%Y/%m/%d'])
+    docfile = models.FileField(upload_to='documents/%Y/%m/%d')
